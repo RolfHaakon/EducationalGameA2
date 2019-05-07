fa9ae745e4b898d7b52b5b90d2b271774ed36493
Rolf <30429371+RolfHaakon@users.noreply.github.com>
2019-05-07 13:27:11 +1000

    Working - time bar working

--- app/src/main/java/com/example/educationalgamea2/GameActivity.java
+++ app/src/main/java/com/example/educationalgamea2/GameActivity.java
@@ -1,14 +1,46 @@
 package com.example.educationalgamea2;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ProgressBar;
 
 public class GameActivity extends AppCompatActivity {
+
+    ProgressBar pb;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        pb = findViewById(R.id.progressbar);
+
+
+        ObjectAnimator animation = ObjectAnimator.ofInt(pb, "progress", 0, 100);
+        animation.setDuration(5000);
+        animation.setInterpolator(new DecelerateInterpolator());
+        animation.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) { }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                //do something when the countdown is complete
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) { }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) { }
+        });
+        animation.start();
+
+
+
+
     }
 }
